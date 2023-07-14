ee19f4d6cf2fa7e54d94fbfab9de10c4c32fa071
cody-and <codya.0807@icloud.com>
2023-07-13 23:48:08 -0500

    added listener to the save button so that it saves the text area to the local storage

--- Develop/script.js
+++ Develop/script.js
@@ -4,7 +4,15 @@
 $(function () {
   var currentDay = dayjs().format("dddd, MMMM D");
   $("#currentDay").text(currentDay);
-  
+
+  $(".saveBtn").on("click", function (){
+    var textarea = $(this).siblings(".description")
+    var eventText = textarea.val()
+    console.log(eventText)
+    var timeBlockId = $(this).parent().attr("#id");
+    localStorage.setItem(timeBlockId, eventText)
+  })
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
