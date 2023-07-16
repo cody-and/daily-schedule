68c9b96e38bbb4affa092e1a02e0b979d98d023a
cody-and <codya.0807@icloud.com>
2023-07-16 13:10:18 -0500

    js now generates the rest of the hours and saves them in local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,55 +1,76 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  var currentDay = dayjs().format("dddd, MMMM D")
+  var currentDay = dayjs().format("dddd, MMMM D");
   $("#currentDay").text(currentDay);
-  
+
   $(".saveBtn").on("click", function () {
     var textarea = $(this).siblings(".description");
     var eventText = textarea.val();
-    var timeblockId = $(this).parent().attr("id")
+    var timeblockId = $(this).parent().attr("id");
     localStorage.setItem(timeblockId, eventText);
   });
 
   var currentHour = dayjs().hour();
+  var startHour = 12;
+  var endHour = 17; 
+
+  for (var i = startHour; i <= endHour; i++) {
+    var timeblockId = "hour-" + i;
+    var displayHour = dayjs().hour(i).format("hA");
+
+    var $timeBlock = $('<div>')
+      .attr("id", timeblockId)
+      .addClass("row time-block")
+      .appendTo(".container-lg");
+
+    $('<div>')
+      .addClass("col-2 col-md-1 hour text-center py-3")
+      .text(displayHour)
+      .appendTo($timeBlock);
+
+    var $description = $('<textarea>')
+      .addClass("col-8 col-md-10 description")
+      .attr("rows", "3")
+      .appendTo($timeBlock);
+
+    var $saveBtn = $('<button>')
+      .addClass("btn saveBtn col-2 col-md-1")
+      .attr("aria-label", "save")
+      .appendTo($timeBlock);
+
+    $('<i>')
+      .addClass("fas fa-save")
+      .attr("aria-hidden", "true")
+      .appendTo($saveBtn);
+
+    var savedEvent = localStorage.getItem(timeblockId);
+    if (savedEvent) {
+      $description.val(savedEvent);
+    }
+  }
+
   $(".time-block").each(function () {
     var timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
 
     if (timeBlockHour < currentHour) {
-      $(this).removeClass("present future").addClass("past")
+      $(this).removeClass("present future").addClass("past");
     } else if (timeBlockHour === currentHour) {
-      $(this).removeClass("past future").addClass("present")
+      $(this).removeClass("past future").addClass("present");
     } else {
-      $(this).removeClass("past present").addClass("future")
+      $(this).removeClass("past present").addClass("future");
     }
   });
 
+  $(".description").on("input", function () {
+    var eventText = $(this).val();
+    var timeblockId = $(this).closest(".time-block").attr("id");
+    localStorage.setItem(timeblockId, eventText);
+  });
 
-  $(".time-block").each(function () {
-    var timeblockId = $(this).attr("id");
+  $(".description").each(function () {
+    var timeblockId = $(this).closest(".time-block").attr("id");
     var savedEvent = localStorage.getItem(timeblockId);
     if (savedEvent) {
-      $(this).find(".description").val(savedEvent);
+      $(this).val(savedEvent);
     }
   });
 });
-  
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
